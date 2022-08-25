04a9a17eee43ccf6040031ec9494a150a08facee
78745364 <111258058+78745364@users.noreply.github.com>
2022-08-24 20:51:18 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+document.getElementById('1').style.textAlign = 'center';
+document.getElementById('2').style.textAlign = 'center';
+document.getElementById('3').style.textAlign = 'center';
+document.getElementById('4').style.textAlign = 'center';
+
+x = 5
+
+function myFunction(element, color) {
+    element.style.color = color;
+    x += 1
+    alert(x)
+  }
