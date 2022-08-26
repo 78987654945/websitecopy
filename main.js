6d6cccbedbc83764b28c0e72f4625657525f3ddc
78745364 <111258058+78745364@users.noreply.github.com>
2022-08-26 07:53:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ document.getElementById('2').style.textAlign = 'center';
 document.getElementById('3').style.textAlign = 'center';
 document.getElementById('4').style.textAlign = 'center';
 
-x = 0
+x1 = 0
+x2 = 0
+x3 = 0
+x4 = 0
+x5 = 0
+x6 = 0
+x7 = 0
+x8 = 0
+x9 = 0
 
 function myFunction(element, color) {
 
